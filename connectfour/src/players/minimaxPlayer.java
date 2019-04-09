78b9acbb2cd41ac1291ba27f869b2edd3bcc24a4
fit217 <chasebrown218@gmail.com>
2019-04-09 12:07:01 -0400

    chooseMove now works

--- connectfour/src/players/minimaxPlayer.java
+++ connectfour/src/players/minimaxPlayer.java
@@ -15,16 +15,14 @@ import sun.misc.Queue;
 
 /**
  * @author chase
- *
  */
-public class minimaxPlayer extends Player{
+public class minimaxPlayer extends Player {
 	private ConnectFourGUI gui_;// GUI
 	private Estimate estimate_;
 	private Stack<Integer> stack_;
 	private static int maxCalled = 0;
 	private static int minCalled = 0;
 
-	
 	/**
 	 * Create a default minimax player.
 	 * 
@@ -33,12 +31,13 @@ public class minimaxPlayer extends Player{
 	 * @param gui
 	 *          the GUI for getting the player's move
 	 */
-	public minimaxPlayer(GamePiece piece, ConnectFourGUI gui) {
-		
+	public minimaxPlayer ( GamePiece piece, ConnectFourGUI gui ) {
+
 		super(piece,Long.MAX_VALUE);
 		gui_ = gui;
-		
+
 	}
+
 	/**
 	 * Create a minimax player with a specific timeout.
 	 * 
@@ -47,20 +46,22 @@ public class minimaxPlayer extends Player{
 	 * @param gui
 	 *          the GUI for getting the player's move
 	 * @param timeOut
-	 * 					the desired time for a move to be made
+	 *          the desired time for a move to be made
 	 */
-	public minimaxPlayer(GamePiece piece, ConnectFourGUI gui, Long timeOut) {
-		super(piece, timeOut);
-		gui_=gui;
-	
+	public minimaxPlayer ( GamePiece piece, ConnectFourGUI gui, Long timeOut ) {
+		super(piece,timeOut);
+		gui_ = gui;
+
 	}
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
 	 * @see game.Player#chooseMove(game.ConnectFourBoard)
 	 */
 	@Override
 	public void chooseMove ( ConnectFourBoard board ) {
-		reset();//resets move and stop at the start of the turn
-		//TODO determine the move
+		reset();// resets move and stop at the start of the turn
+		// TODO determine the move
 		int choice = 3;
 		System.out.println(super.getTimeout());
 		try {
@@ -68,38 +69,52 @@ public class minimaxPlayer extends Player{
 		} catch ( GameRuleViolation e ) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
+		}
 		System.out.println("Player is making a move");
-		move_=new Move(piece_,choice,false);
+		move_ = new Move(piece_,choice,false);
+		System.out.print("Move is at column " + choice);
 	}
-	public int miniMax(ConnectFourBoard board, int depth) throws GameRuleViolation {
-			double best = 3;
-			long currentTime = 0;
-			while(depth != 2) {
+
+	public int miniMax ( ConnectFourBoard board, int depth )
+	    throws GameRuleViolation {
+
+		double best = 3;
+
+		while ( depth != 2 ) {
+			System.out.println(stop_ +"___________________________________________________-");
+			if ( !stop_ ) {
+				
 				best = maxValue(board,0,depth);
 				System.out.println(best);
 				minCalled = 0;
 				maxCalled = 0;
 				depth++;
 			}
-			return (int) best;
+			else
+				break;
 		}
-	
-	private double maxValue(ConnectFourBoard state,int level, int depth) {
-		//System.out.println(level);
+		return (int) best;
+
+	}
+
+	private double maxValue ( ConnectFourBoard state, int level, int depth ) {
+		// System.out.println(level);
+		if(stop_) {
+			return new Estimate(this).h(state,piece_);
+		}
 		maxCalled++;
 		System.out.println("max called " + maxCalled);
-		if(level == depth) {
-			//System.out.println(new Estimate(this).h(state,piece_));
+		if ( level == depth ) {
+			// System.out.println(new Estimate(this).h(state,piece_));
 			System.out.println("Terminal state");
 			return new Estimate(this).h(state,piece_);
 		}
 		double v = Double.NEGATIVE_INFINITY;
 		double best = 3;
-		for(int i = 0; i < 7;i++) {
+		for ( int i = 0 ; i < 7 ; i++ ) {
 			ConnectFourBoard newState = state.copy();
-			if(state.isFull(i)) {
-				//System.out.println("board is full in col " + i);
+			if ( state.isFull(i) ) {
+				// System.out.println("board is full in col " + i);
 				continue;
 			}
 			try {
@@ -110,67 +125,64 @@ public class minimaxPlayer extends Player{
 				continue;
 			}
 			double temp = minValue(newState,level++,depth);
-		  System.out.println(temp + " vs " + v);
-			if(v < temp) {
+			System.out.println(temp + " vs " + v);
+			if ( v < temp ) {
 				v = temp;
 				best = i;
 			}
 
 			v = Math.max(v,minValue(newState,level++,depth));
 		}
-		if(level == 1) {
+		if ( level == 1 ) {
 			return best;
-		}else {
+		} else {
 			return v;
 		}
-//		return v;
+		// return v;
 	}
-		
-		private double minValue(ConnectFourBoard state,int level, int depth) {
-			minCalled++;
-			System.out.println("min called " + minCalled);
-			//System.out.println(level);
-			if(level == depth) {
-				//System.out.println(new Estimate(this).h(state,piece_.other()));
-				System.out.println("Terminal state");
-				return new Estimate(this).h(state,piece_.other());
-			}
-			double best = 3;
-			double v = Double.POSITIVE_INFINITY;
-			for(int i = 0; i < 7;i++) {
-				ConnectFourBoard newState = state.copy();
-				if(newState.isFull(i)) {
-					//System.out.println("board is full in col " + i);
-					continue;
-				}
-				try {
-					newState.drop(piece_.other(),i);
-				} catch ( GameRuleViolation e ) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					continue;
-				}
-				double temp = maxValue(newState,level++,depth);
-			  System.out.println(temp + " vs " + v);
-				if(v > temp) {
-					v = temp;
-					best = i;
-				}
 
-				//v = Math.min(v,maxValue(newState,level++,depth));
-			}
-			if(level == 1) {
-				return best;
-			}else {
-				return v;
-			}
-			//return v;
+	private double minValue ( ConnectFourBoard state, int level, int depth ) {
+		if(stop_) {
+			return new Estimate(this).h(state,piece_.other());
 		}
+		minCalled++;
+		System.out.println("min called " + minCalled);
+		// System.out.println(level);
+		if ( level == depth ) {
+			// System.out.println(new Estimate(this).h(state,piece_.other()));
+			System.out.println("Terminal state");
+			return new Estimate(this).h(state,piece_.other());
+		}
+		double best = 3;
+		double v = Double.POSITIVE_INFINITY;
+		for ( int i = 0 ; i < 7 ; i++ ) {
+			ConnectFourBoard newState = state.copy();
+			if ( newState.isFull(i) ) {
+				// System.out.println("board is full in col " + i);
+				continue;
+			}
+			try {
+				newState.drop(piece_.other(),i);
+			} catch ( GameRuleViolation e ) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				continue;
+			}
+			double temp = maxValue(newState,level++,depth);
+			System.out.println(temp + " vs " + v);
+			if ( v > temp ) {
+				v = temp;
+				best = i;
+			}
 
+			// v = Math.min(v,maxValue(newState,level++,depth));
+		}
+		if ( level == 1 ) {
+			return best;
+		} else {
+			return v;
+		}
+		// return v;
+	}
 
-
-		
-	
-
-	
 }
